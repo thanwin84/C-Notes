34203b0af2154a33ca4d6c9f01285d900c5cfdf6
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-06-25 22:12:39 +0600

    Create sets.cpp

---
+++ sets/sets.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include<set>
+#include <unordered_set>
+#include<unordered_map>
+#include<string>
+#define mod 1000000007;
+using namespace std;
+
+void print_set(set<string> s) {
+	for (auto it : s) {
+		cout << it << endl;
+	}
+}
+void print_unoredred_set(unordered_set<string> &s) {
+	for (auto it : s) {
+		cout << it << endl;
+	}
+}
+
+int main() {
+	set<string> s;
+	s.insert("abc");
+	s.insert("kdht");
+	s.insert("bad");
+	s.insert("abc");
+	
+	// checking if item exists
+	/*auto it = s.find("abk");
+	cout << (*it) << endl;*/
+	auto it = s.find("abclo");
+	if (it != s.end()) {
+		s.erase(it);
+	}
+	unordered_set<string> us;
+	us.insert("abc");
+	us.insert("kdht");
+	us.insert("bad");
+	us.insert("abc");
+	auto temp = us.find("abcki");
+	if (temp != us.end()) {
+		cout << "Found" << endl;
+	}
+
+	//print_unoredred_set(us);
+	multiset<string> ms;
+	ms.insert("abc");
+	ms.insert("abc");
+	ms.insert("kdh");
+	ms.insert("bck");
+	for (auto it : ms) {
+		cout << it << endl;
+	}
+	// it wil delete all abc
+	ms.erase("abc");
+	return 0;
+}
